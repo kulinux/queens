22935d0466a062ccd12476ea58f393814241abc6
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2024-09-07 07:54:21 +0200

    before next in board with tryiedBoards

--- src/test/scala/WalkerWithRollbackShould.scala
+++ src/test/scala/WalkerWithRollbackShould.scala
@@ -3,16 +3,16 @@ import org.scalatest.funsuite.AnyFunSuite
 import cats.data.State
 import org.scalatest.matchers.should.Matchers
 
-case class Node(board: Board, parent: Option[Node])
+case class Node(board: Board, parent: Option[Node], tryiedBoards: Seq[Board])
 
 object StateUtilWithRollback {
   val checker = Checker()
   val nextQueenInBoard = NextQueenInBoard(checker)
   def nextNode(node: Node): Node = {
-    println(s"next node ${node}")
+    val nextTryiedBoards = node.tryiedBoards :+ node.board
     nextQueenInBoard.next(node.board) match {
-      case None => Node(node.parent.get.board, node.parent.get.parent)
-      case Some(found) => Node(node.board.add(found), Some(node))
+      case None => Node(node.parent.get.board, node.parent.get.parent, Seq())
+      case Some(found) => Node(node.board.add(found), Some(node), Seq())
     }
   }
 }
@@ -25,21 +25,18 @@ class WalkerWithRollbackShould extends AnyFunSuite with MockFactory with Matcher
     val emptyBoard = Board.empty()
     val expectedBoard = Board(Seq(Queen(0, 0)))
 
-    val initial = Node(emptyBoard, None)
-    val expected = Node(expectedBoard, None)
+    val initial = Node(emptyBoard, None, Seq())
     
-
-    state.run(initial).value._1 shouldBe expected
+    state.run(initial).value._1.board shouldBe expectedBoard
   }
 
   test("one queen") {
     val oneQueen = Board(Seq(Queen(0, 0)))
     val expectedBoard = Board(Seq(Queen(0, 0), Queen(2, 1)))
 
-    val initial = Node(oneQueen, None)
-    val expected = Node(expectedBoard, None)
+    val initial = Node(oneQueen, None, Seq())
 
-    state.run(initial).value._1 shouldBe expected
+    state.run(initial).value._1.board shouldBe expectedBoard
   }
 
   test("rollback") {
@@ -63,8 +60,8 @@ class WalkerWithRollbackShould extends AnyFunSuite with MockFactory with Matcher
 
     val expectedBoard = Board(Seq(Queen(0, 0), Queen(2, 1)))
 
-    val parentNode = Node(parentBoard, None)
-    val initial = Node(initialBoard, Some(parentNode))
+    val parentNode = Node(parentBoard, None, Seq())
+    val initial = Node(initialBoard, Some(parentNode), Seq())
     val expected = parentNode
 
     state.run(initial).value._1 shouldBe expected
@@ -94,6 +91,7 @@ class WalkerWithRollbackShould extends AnyFunSuite with MockFactory with Matcher
           Queen(3, 5), 
         )
       )
+    /*
     val expectedParent =
       Some(Node(Board(List(Queen(0, 0), Queen(2, 1), Queen(1, 2), Queen(5, 3), Queen(7, 4))),
         Some(Node(Board(List(Queen(0, 0), Queen(2, 1), Queen(1, 2), Queen(5, 3))),
@@ -106,11 +104,12 @@ class WalkerWithRollbackShould extends AnyFunSuite with MockFactory with Matcher
           ))
         ))
     ))
+    */
 
-    val initial = Node(emptyBoard, None)
-    val expected = Node(expectedBoard, expectedParent)
+    val initial = Node(emptyBoard, None, Seq())
 
-    newState.run(initial).value._1 shouldBe expected
+    newState.run(initial).value._1.board shouldBe expectedBoard
   }
+
 }
 
